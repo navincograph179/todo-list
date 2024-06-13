ec0da54d6a99bb5b8b8a87790b9a5cac26daa34f
navincograph179 <navin_gurung@co-graph.com>
2024-06-13 16:52:17 +0900

    add some feature like add and delete

--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -1,60 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DataTable = () => {
+  const [formData, setFromData] = useState({ name: "", gender: "", age: "" });
+  const [data, setData] = useState([]);
+  const [editId, setEditIt] = useState(false);
+
+  const handleInputChange = (e) => {
+    setFromData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleAddClick = () => {
+    if (formData.name && formData.gender && formData.age) {
+      const newItem = {
+        id: Date.now(),
+        name: formData.name,
+        gender: formData.gender,
+        age: formData.age,
+      };
+      setData([...data, newItem]);
+      setFromData({ name: "", gender: "", age: "" });
+    }
+  };
+  const handleDelete = (id) => {
+    const updatedList = data.filter((item) => item.id !== id);
+    setData(updatedList);
+  };
+
   return (
-    <div className="container">
-      <div className="add-container">
-        <div className="info-container">
-        {/* This line for name input */}
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            value={""}
-            onChange={() => {}}
-          />
-          {/* This line for gender input */}
-           <input
-            type="text"
-            placeholder="Gender"
-            name="gender"
-            value={""}
-            onChange={() => {}}
-          />
-          {/* This line for age input */}
-           <input
-            type="text"
-            placeholder="Age"
-            name="age"
-            value={""}
-            onChange={() => {}}
-          />
-
+    <div className="box">
+      <div className="container">
+        <div className="add-container">
+          <div className="info-container">
+            {/* This line for name input */}
+            <input
+              type="text"
+              placeholder="名前"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+            {/* This line for gender input */}
+            <input
+              type="text"
+              placeholder="性別"
+              name="gender"
+              value={formData.gender}
+              onChange={handleInputChange}
+            />
+            {/* This line for age input */}
+            <input
+              type="text"
+              placeholder="年齢"
+              name="age"
+              value={formData.age}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button className="add" onClick={handleAddClick}>
+            追加
+          </button>
         </div>
-        <button className="add">ADD</button>
-      </div>
 
-      <div className="search-table-container">
-        {/* This line for search by name input */}
-      <input
+        <div className="search-table-container">
+          {/* This line for search by name input */}
+          <input
+            className="search-input"
             type="text"
-            placeholder="Search by name"
+            placeholder="名前で検索"
             value={""}
             onChange={() => {}}
           />
-        <table>
+          <table>
             <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Gender</th>
-                    <th>Age</th>
-                    <th>Action</th>
-                </tr>
+              <tr>
+                <th>名前</th>
+                <th>性別</th>
+                <th>年齢</th>
+                <th>Action</th>
+              </tr>
             </thead>
 
-            <tbody></tbody>
-        </table>
-        <div className="pagination"></div>
+            <tbody>
+              {data.map((item) => (
+                <tr>
+                  <td key={item.id} contentEditable = {editId=== item.id}>{item.name}</td>
+                  <td key={item.id} contentEditable = {editId=== item.id}>{item.gender}</td>
+                  <td key={item.id} contentEditable = {editId=== item.id}>{item.age}</td>
+                  <td className="actions">
+                    <button className="edit" onClick={()=> setEditIt(item.id)}
+                    >編集</button>
+                    <button
+                      className="delete"
+                      onClick={() => handleDelete(item.id)}
+                    >
+                      解除
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="pagination"></div>
+        </div>
       </div>
     </div>
   );
