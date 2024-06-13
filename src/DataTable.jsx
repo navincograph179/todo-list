28597e0f11ec805ec4edddb6e96e0c332321c911
navincograph179 <navin_gurung@co-graph.com>
2024-06-13 13:36:10 +0900

    some modified files and add features

---
+++ src/DataTable.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DataTable = () => {
+  return (
+    <div className="container">
+        <div className="add-container">
+            
+        </div>
+
+
+        <div className="search-table-container"></div>
+    </div>
+  )
+}
+
+export default DataTable;
